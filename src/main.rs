0407bc1116993d9ffa3017e7c1937c64d5412cb3
Crides <zhuhaoqing@live.cn>
2020-02-05 20:51:34 -0600

    sync

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use sched_test::*;
+use dirs::home_dir;
 
 fn main() {
     let ctx = script::ScriptContext::new();
@@ -6,7 +7,9 @@ fn main() {
         eprintln!("{}", e);
         return;
     };
-    if let Err(e) = ctx.run_file("/home/steven/.config/sched/init.lua") {
+    let mut init_file = home_dir().unwrap();
+    init_file.push(".config/sched/init.lua");
+    if let Err(e) = ctx.run_file(init_file) {
         eprintln!("{}", e);
         return;
     };
